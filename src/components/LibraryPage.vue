bd09873349572699766d1cd18444db3696775f40
Ata Gowani <atagowani@gmail.com>
2022-07-29 23:14:21 -0500

    Fix xs library responsiveness

--- src/components/LibraryPage.vue
+++ src/components/LibraryPage.vue
@@ -4,100 +4,100 @@
     <p class="lead">some of the books i've enjoyed reading</p>
   </div>
   <div class="d-flex flex-row flex-wrap books justify-content-center">
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img src="../../src/assets/books/1984.webp" alt="1984" />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/the_alchemist.jpeg"
         alt="The Alchemist"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/atomic_habits.jpeg"
         alt="Atomic Habits"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/harry_potter_3.jpeg"
         alt="Harry Potter 3"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/harry_potter_2.jpeg"
         alt="Harry Potter 2"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/harry_potter_1.jpeg"
         alt="Harry Potter 1"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/on_the_shortness_of_life.jpg"
         alt="On The Shortness of Life"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/the_next_person_you_meet_in_heaven.jpeg"
         alt="The Next Person You Meet In Heaven"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/forty_rules_of_love.jpeg"
         alt="Forty Rules of Love"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/tuesdays_with_morrie.jpeg"
         alt="Tuesdays With Morrie"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/the_five_people_you_meet_in_heaven.jpeg"
         alt="The Five People You Meet In Heaven"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/the_alchemy_of_happiness.jpeg"
         alt="The Alchemy of Happiness"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/i_cant_make_this_up.jpeg"
         alt="I Can't Make This Up"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/saint_anything.jpg"
         alt="Saint Anything"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/the_drunkards_walk.jpeg"
         alt="The Drunkards Walk"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/the_glass_castle.jpg"
         alt="The Glass Castle"
       />
     </div>
-    <div class="col-6 col-sm-4 col-lg-2 text-center">
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img
         src="../../src/assets/books/a_long_way_gone.jpeg"
         alt="A Long Way Gone"
