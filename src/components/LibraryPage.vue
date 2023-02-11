ab2c1e0938a34acfb6d79566b6a19ab55272bedb
Ata Gowani <atagowani@gmail.com>
2023-02-11 03:07:23 -0600

    Add new book

--- src/components/LibraryPage.vue
+++ src/components/LibraryPage.vue
@@ -11,6 +11,12 @@
       />
     </div> -->
     <div class="col-12 col-sm-4 col-lg-2 text-center">
+      <img
+        src="../../src/assets/books/harry_potter_4.jpeg"
+        alt="Harry Potter 4"
+      />
+    </div>
+    <div class="col-12 col-sm-4 col-lg-2 text-center">
       <img src="../../src/assets/books/brida.jpeg" alt="Brida" />
     </div>
     <div class="col-12 col-sm-4 col-lg-2 text-center">
