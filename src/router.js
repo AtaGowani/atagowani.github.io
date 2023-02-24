f82a23ab37b74408bb7d06a6fba2e3db37552372
Ata Gowani <atagowani@gmail.com>
2023-02-24 14:06:58 -0600

    Remove unused components

--- src/router.js
+++ src/router.js
@@ -1,9 +1,7 @@
 import { createWebHashHistory, createRouter } from 'vue-router';
 import NotFound from "@/components/NotFound.vue";
 import Home from '@/components/Home.vue';
-import BlogPage from '@/components/BlogPage.vue';
 import LibraryPage from '@/components/LibraryPage.vue';
-import BlogContentPage from '@/components/BlogContentPage.vue';
 import AlbumPage from '@/components/AlbumPage.vue';
 
 const routes = [
@@ -11,14 +9,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
-  }, {
-    path: '/blog',
-    name: 'Blog',
-    component: BlogPage,
-  }, {
-    path: '/blog/:id',
-    name: 'BlogContent',
-    component: BlogContentPage,
   }, {
     path: '/library',
     name: 'Library',
